e9ffd589d60ef1816dcb5322d9f09078b852cd82
Jon Cagle <joncagle@unisyntechnologies.com>
2026-03-09 07:17:19 -0500

    feat: added netlify integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,20 @@
 import { defineConfig, passthroughImageService } from 'astro/config';
 import tailwind from '@tailwindcss/vite';
 
+import netlify from '@astrojs/netlify';
+
 export default defineConfig({
   integrations: [],
+
   vite: {
     plugins: [tailwind()],
   },
+
   site: 'https://www.rantoulhistoricalsociety.org',
+
   image: {
     service: passthroughImageService(),
   },
+
+  adapter: netlify(),
 });
